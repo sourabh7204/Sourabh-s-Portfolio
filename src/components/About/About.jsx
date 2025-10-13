2d81dd2873abb1109df5c9b6b1d9b778c492acb2
sourabh7204 <sourabh722004@gmail.com>
2025-10-13 18:59:30 +0530

    Updated Resume

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -56,7 +56,7 @@ const About = () => {
 
           {/* Resume Button */}
           <a
-            href="https://drive.google.com/file/d/1E_u_yInBWKAMN7RpkP74fFuUEi0tHKsv/view?usp=drivesdk"
+            href="https://drive.google.com/file/d/1AYEcZ5h81HyIriNVEKEisxV36otoqHi5/view?usp=drivesdk"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105 relative overflow-hidden"
